b11aacbce0395253d1374d40af4124874949ecf5
codeswithrohit <hrohit320@gmail.com>
2024-04-02 09:35:09 +0530

    submitted changes

---
+++ components/Recentlyupdated.js
@@ -0,0 +1,52 @@
+import React from 'react';
+
+const Recentlyupdated = () => {
+  return (
+    <div className='px-12' >
+      <div className="w-80 bg-white shadow rounded">
+        <div className="h-48 w-full bg-gray-200 flex flex-col justify-between p-4 bg-cover bg-center"
+          style={{backgroundImage: "url('https://gta5modaz.com/wp-content/uploads/2022/09/gta_5_mods_Muscle_orange_Roblox_Rainbow_Friend_thumb-280x280.jpg')"}}>
+          
+          <div>
+            <span className="uppercase text-xs bg-green-50 p-0.5 border-green-500 border rounded text-green-700 font-medium select-none">
+              co-owner
+            </span>
+          </div>
+        </div>
+        <div className="p-4 flex flex-col items-center">
+          <p className="text-gray-400 font-light text-xs text-center">GTA 5 mods gta_5_mods_Muscle_orange_Roblox_Rainbow_Friend_thumb</p>
+          {/* <h1 className="text-gray-800 text-center mt-1">Item name</h1> */}
+          <p className="text-center text-gray-800 mt-1">€1299</p>
+          {/* <div className="inline-flex items-center mt-2">
+            <button className="bg-white rounded-l border text-gray-600 hover:bg-gray-100 active:bg-gray-200 disabled:opacity-50 inline-flex items-center px-2 py-1 border-r border-gray-200">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-4" fill="none" viewBox="0 0 24 24"
+                stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M20 12H4" />
+              </svg>
+            </button>
+            <div className="bg-gray-100 border-t border-b border-gray-100 text-gray-600 hover:bg-gray-100 inline-flex items-center px-4 py-1 select-none">
+              2
+            </div>
+            <button className="bg-white rounded-r border text-gray-600 hover:bg-gray-100 active:bg-gray-200 disabled:opacity-50 inline-flex items-center px-2 py-1 border-r border-gray-200">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-4" fill="none" viewBox="0 0 24 24"
+                stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 4v16m8-8H4" />
+              </svg>
+            </button>
+          </div> */}
+
+          <button className="py-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-600 active:bg-blue-700 disabled:opacity-50 mt-4 w-full flex items-center justify-center">
+            Add to Cart
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 ml-2" fill="none" viewBox="0 0 24 24"
+              stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+            </svg>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Recentlyupdated;
